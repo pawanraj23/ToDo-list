586178f0a9c7c00b084838706d3bd2fa66112fd6
Pawan Raj Polaki <ppolaki@ciberspring.com>
2025-04-22 19:20:22 +0530

    adding id to listitem

--- js/todo.js
+++ js/todo.js
@@ -2,7 +2,9 @@ let inputText = document.getElementById("inputText");
 const resultSection = document.getElementById("result");
 
 function addListItems(inputText) {
+    addListItems.counter = (addListItems.counter || 0) + 1;
     const listItem = document.createElement("li");
+    listItem.id = `item-${addListItems.counter}`;
     listItem.textContent = inputText;
     let ul = resultSection.querySelector("ul");
     if (!ul) {
